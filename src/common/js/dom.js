ab4c7cc760ce665151f7a1756eee964fc4bf05c1
Star <mx_c@foxmail.com>
2018-07-05 17:09:37 +0800

    add singer as songs list

--- src/common/js/dom.js
+++ src/common/js/dom.js
@@ -1,20 +1,52 @@
-export function addClass(el,className){
-  if(hasClass(el,className))return;
-  let classList=el.className.split(' ');
+export function addClass(el, className) {
+  if (hasClass(el, className)) return;
+  let classList = el.className.split(' ');
   classList.push(className);
-  el.className=classList.join(' ')
+  el.className = classList.join(' ')
 }
-export function hasClass(el,className){
-  let reg=new RegExp('(^|\\s)'+className+'(\\s|^)');
+export function hasClass(el, className) {
+  let reg = new RegExp('(^|\\s)' + className + '(\\s|^)');
   return reg.test(el.className)
 }
 
-export function getData(el,name,val){
-  const prefix='data-';
-  name=prefix+name;
-  if(val){
-    return el.setAttribute(name,val)
-  }else{
+export function getData(el, name, val) {
+  const prefix = 'data-';
+  name = prefix + name;
+  if (val) {
+    return el.setAttribute(name, val)
+  } else {
     return el.getAttribute(name)
   }
+}
+
+let elementStyle = document.createElement('div').style
+
+let vendor = (() => {
+  let transformNames = {
+    webkit: 'webkitTransform',
+    Moz: 'MozTransform',
+    O: 'OTransform',
+    ms: 'msTransform',
+    standard: 'transform'
+  }
+
+  for (let key in transformNames) {
+    if (elementStyle[transformNames[key]] !== undefined) {
+      return key
+    }
+  }
+
+  return false
+})()
+
+export function prefixStyle(style) {
+  if (vendor === false) {
+    return false
+  }
+
+  if (vendor === 'standard') {
+    return style
+  }
+
+  return vendor + style.charAt(0).toUpperCase() + style.substr(1)
 }
--- src/components/singer-detail/singer-detail.vue
+++ src/components/singer-detail/singer-detail.vue
@@ -1,71 +1,70 @@
 <template>
     <transition name='slide'> 
-        <div class="singer-detail">
-            singer-detail
-        </div>
+        <music-list :songs='songs' :title='title' :bg-image='bgImage'></music-list>
     </transition>
 </template>
 
 <script type="text/ecmascript-6">
-import {mapGetters} from 'vuex'
+import { mapGetters } from "vuex";
 import { getSingerDetail } from "api/singer";
 import { createSong } from "common/js/song";
-import {ERR_OK} from 'api/config'
+import { ERR_OK } from "api/config";
+import MusicList from "components/music-list/music-list";
 export default {
-    data(){
-      return {
-        songs: []
+  data() {
+    return {
+      songs: []
+    };
+  },
+  computed: {
+    title() {
+      return this.singer.name;
+    },
+    bgImage() {
+      return this.singer.avatar;
+    },
+    ...mapGetters(["singer"])
+  },
+  created() {
+    this._getDetail();
+  },
+  methods: {
+    _getDetail() {
+      if (!this.singer.id) {
+        this.$router.push("/singer");
+        return;
       }
-    },
-    computed:{
-        ...mapGetters([
-            'singer'
-        ])
-    },
-    created(){
-        this._getDetail()
-    },
-    methods:{
-        _getDetail(){
-            if(!this.singer.id){
-                this.$router.push('/singer')
-                return;
-            }
-            getSingerDetail(this.singer.id).then((res)=>{
-                if(res.code===ERR_OK){
-                    this.songs=this._normalizeSong(res.data.list)
-                }
-                console.log(this.songs)
-            });
-        },
-        _normalizeSong(list){
-            let ret=[]
-            list.forEach((item) => {
-                let {musicData} = item
-                if (musicData.songid && musicData.albummid) {
-                    ret.push(createSong(musicData))
-                }
-            });
-            return ret;
+      getSingerDetail(this.singer.id).then(res => {
+        if (res.code === ERR_OK) {
+          this.songs = this._normalizeSong(res.data.list);
         }
+      });
+    },
+    _normalizeSong(list) {
+      let ret = [];
+      list.forEach(item => {
+        let { musicData } = item;
+        if (musicData.songid && musicData.albummid) {
+          ret.push(createSong(musicData));
+        }
+      });
+      return ret;
     }
-}
+  },
+  components: {
+    MusicList
+  }
+};
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style  scoped lang="stylus" rel="stylesheet/stylus">
-    @import '~common/stylus/variable';
-    .singer-detail
-        position: fixed
-        z-index: 100
-        top:0
-        left:0
-        right:0
-        bottom:0
-        background:$color-background
-    .slide-enter-active, .slide-leave-active
-        transition: all 0.3s
+@import '~common/stylus/variable';
 
-    .slide-enter, .slide-leave-to
-        transform: translate3d(100%, 0, 0)
+.slide-enter-active, .slide-leave-active {
+    transition: all 0.3s;
+}
+
+.slide-enter, .slide-leave-to {
+    transform: translate3d(100%, 0, 0);
+}
 </style>
