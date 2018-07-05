e657ab265389990ce9d9b96b138f5ccafe189072
Star <mx_c@foxmail.com>
2018-07-05 17:53:50 +0800

    nothing

--- src/store/state.js
+++ src/store/state.js
@@ -1,7 +1,14 @@
+import { playMode } from "common/js/config";
 const state={
   singer:{
 
-  }
+  },
+  playing:false,
+  fullScreem:false,
+  playList:[],
+  sequenceList:[],
+  mode:playMode.sequence,
+  currentIndex:-1
 }
 
 export default state;
