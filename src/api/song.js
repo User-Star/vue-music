568efd6c369ffefd3921b4075c24adcc032342ca
User-Star <mx_c@foxmail.com>
2018-07-20 15:40:21 +0800

    nothing

--- src/api/song.js
+++ src/api/song.js
@@ -36,7 +36,7 @@ export function getMusicUrl(musicData) {
     uin: 0,
     songmid: musicData.mid,
     filename: `C400${musicData.mid}.m4a`,
-    guid: 3360709558
+    guid: 4947008496
   })
 
   return axios.get(url, {
@@ -47,8 +47,14 @@ export function getMusicUrl(musicData) {
       code: res.data.code,
       userip: res.data.userip,
       data: {
-        musicUrl: `http://dl.stream.qqmusic.qq.com/${res.data.data.items[0].filename}?vkey=${res.data.data.items[0].vkey}&guid=3360709558&uin=0&fromtag=66`
+        musicUrl: `http://dl.stream.qqmusic.qq.com/${res.data.data.items[0].filename}?vkey=${res.data.data.items[0].vkey}&guid=4947008496&uin=3248&fromtag=38`
       }
     })
   })
 }
+
+//https://u.y.qq.com/cgi-bin/musicu.fcg?_=1532071766284
+
+//http://dl.stream.qqmusic.qq.com/C400103r8zKA1NRSNz.m4a?guid=4947008496&vkey=2921228B004A03FED187F729B389684B1769A2033E08492C3F05F12484A949216A6E294C3C1366528701DEC7DC2E80AD7141899AE2A7A087&uin=3248&fromtag=38
+
+//http://dl.stream.qqmusic.qq.com/C400003r8zKA1NRSNz.m4a?vkey=4BBC07CE592AA230B0CD45D75871074D25987DFFDBB4315B26BCBC5356386FE96405EEDF7B015DDF716EBF7A31F33A2C7B06A42E8360009A&guid=4947008496&uin=0&fromtag=38
